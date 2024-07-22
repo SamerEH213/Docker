65da89c6c404e77feaa99dd34ab7023782619a20
Samer Harb <125601349+SamerEH213@users.noreply.github.com>
2024-07-22 22:13:55 +0300

    Update airflow-entrypoint.sh

--- airflow/scripts/airflow-entrypoint.sh
+++ airflow/scripts/airflow-entrypoint.sh
@@ -2,6 +2,6 @@
 airflow resetdb
 airflow db init
 airflow upgradedb
-airflow users create -r Admin -u airflow -e sameremadharb213@gmail.com -f samer -l harb -p test1234
+airflow users create -r Admin -u username -e your_email -f first_name -l last_name -p pass
 airflow scheduler &
 airflow webserver
